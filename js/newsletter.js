e7ba7b77c46196a2eacd716f26683496bfa7d9b9
nath54 <=>
2020-06-30 18:03:16 +0200

    aa

---
+++ js/newsletter.js
@@ -0,0 +1,15 @@
+
+var dispo=false;
+
+function send_email_to_newsletter(inpid){
+    email=document.getElementById(inpid);
+    if(!dispo){
+        alert("Cette fonctionnalité n'est pas encore disponible.");
+    }
+
+}
+
+
+
+
+
