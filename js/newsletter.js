789ea6bd0a372b218160038fb536c1070aa05a32
nath54 <=>
2020-06-30 18:15:58 +0200

    aa

--- js/newsletter.js
+++ js/newsletter.js
@@ -1,12 +1,22 @@
 
-var dispo=false;
+var dispo_newsletter=false;
+var dispo_sendmessage=false;
 
 function send_email_to_newsletter(inpid){
-    email=document.getElementById(inpid);
-    if(!dispo){
+    email=document.getElementById(inpid).value;
+    if(!dispo_newsletter){
         alert("Cette fonctionnalité n'est pas encore disponible.");
     }
 
+}
+
+function send_message(){
+    email=document.getElementById("input_email").value;
+    object=document.getElementById("input_object").value;
+    texte=document.getElementById("input_texte").value;
+    if(!dispo_sendmessage){
+        alert("Cette fonctionnalité n'est pas encore disponible.");
+    }
 }
 
 
