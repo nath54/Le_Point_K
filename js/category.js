da4b75ccb3dc133f080be36ba9f03574ef30188b
nathan cerisara <cerisara.nathan@gmail.com>
2020-12-17 21:46:13 +0100

    amelioration

--- js/category.js
+++ js/category.js
@@ -8,15 +8,17 @@ var categories = {
 
 function init_cat(){
     var cat = 1;
-    var parameters = location.search.substring(1).split("&");
-    if(parameters.length>=1){
-        cat = parseInt(parameters[0]);
-    }
-    else{
-        window.location.href="main.html"
+    if(window.location.href.startsWith("category.html")){
+        var parameters = location.search.substring(1).split("&");
+        if(parameters.length>=1){
+            cat = parseInt(parameters[0]);
+        }
+        else{
+            window.location.href="main.html"
+        }
+        document.getElementById("title").innerHTML = "Le Point-K : " + categories[cat].nom;
     }
     //
-    document.getElementById("title").innerHTML = "Le Point-K : " + categories[cat].nom;
     for(elt of document.getElementsByClassName("ncat1")){
         elt.innerHTML = categories[1].nom;
     }
@@ -34,8 +36,10 @@ function init_cat(){
         elt.innerHTML = categories[cat].nom;
     }
     //
-    var d = document.getElementById("row_posts_recent");
-    d.setAttribute("id", "row_cat"+cat+"_posts");
+    if(document.getElementById("row_posts_recent") != undefined){
+        var d = document.getElementById("row_posts_recent");
+        d.setAttribute("id", "row_cat"+cat+"_posts");
+    }
 
     // On finit par poster les articles
     post_articles();
