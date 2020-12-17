3305f3470080605ffb2bf406008fcaa39de8b1c0
nathan cerisara <cerisara.nathan@gmail.com>
2020-12-17 22:56:27 +0100

    ameliorations

--- js/article.js
+++ js/article.js
@@ -8,45 +8,24 @@ else{
 }
 window.idart=id_article;
 
-const lste_eco=["écologie","ecologie","ecology"];
-const lste_pol=["politique"];
-const lste_inf=["sciences","science"];
-const lste_san=["sante","santé"];
 
+var categories = {
+    1: {"nom": "Écologie"},
+    2: {"nom": "Politique"},
+    3: {"nom": "Sciences et Santé"},
+    4: {"nom": "Culture"}
+}
 
 function load_articles(){
     if(article_contents[id_article]!=undefined){
         art=article_contents[id_article];
+        /*
         document.getElementById("titre_h1").innerHTML=art["dossier"];
         document.getElementById("titre_li").innerHTML=art["dossier"];
-        if(lste_eco.includes(art["category"])){
-            document.getElementById("cat_li").innerHTML="écologie";
-            document.getElementById("cat_li").setAttribute("href","../category_ecologie.html");
-        }
-        else if(lste_pol.includes(art["category"])){
-            document.getElementById("cat_li").innerHTML="politique";
-            document.getElementById("cat_li").setAttribute("href","../category_politique.html");
-        }
-        else if(lste_inf.includes(art["category"])){
-            document.getElementById("cat_li").innerHTML="science";
-            document.getElementById("cat_li").setAttribute("href","../category_science.html");
-        }
-        else if(lste_san.includes(art["category"])){
-            document.getElementById("cat_li").innerHTML="santé";
-            document.getElementById("cat_li").setAttribute("href","../category_sante.html");
-        }
-        
-        $("#contenu_de_larticle").load(art["content"]); 
-        
-        try{
-            websocket.send( JSON.stringify({"action":"read_article","id":id_article}) )
-            console.log("art_vu");
-        }
-        catch{
-            console.log("error");
-            //console.log("websocket : ",websocket);
-        }
-        
+        */
+        document.getElementById("cat_li").innerHTML=categories[art["category"]]["nom"];
+        document.getElementById("cat_li").setAttribute("href", "../category.html?"+art["category"]);
+        $("#contenu_de_larticle").load(art["content"]);
     }
 
 }
